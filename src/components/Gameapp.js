854e6e59d01e8eba8085af32278e6581ba2a2384
Hoorieh-1989 <69381842+Hoorieh-1989@users.noreply.github.com>
2022-09-01 12:08:51 +0200

    animals

--- src/components/Gameapp.js
+++ src/components/Gameapp.js
@@ -41,7 +41,7 @@ const App = () => {
 
     const checkForRowOfFour = () => {
         for (let i = 0; i < 64; i++) {
-            const rowOfFour = [i, i + 1, i + 2, i + 3]
+            const rowOfFour = [i, i + 1, i + 2, i + 3,i + 4]
             const decidedColor = currentColorArrangement[i]
             const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
             const isBlank = currentColorArrangement[i] === blank
