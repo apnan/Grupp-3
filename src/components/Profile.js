376d147c9a795a9aae66dfc5c92b1842db184444
Bhavani Palla <bhavani.palla@gmail.com>
2022-09-07 16:56:35 +0200

    removed empty folder

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./profile.css";
 import { updateLoggedIn } from "./context/ProtectedRoutes";
 
 
 const Profile = (handleLogin) => {
+    const [image, setImage] = useState([]);
+
   const handleLogout = (e) => {
     e.preventDefault();
     // handleLogin(false)
     updateLoggedIn();
   };
+useEffect(() => {
+  const fetchImages = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/images");
+      const data = await response.json();
+      setImage(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  fetchImages();
+}, []);
+
+
   return (
     <div className="content">
       <div className="profile">
