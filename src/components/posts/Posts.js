aac3257ca6939596f41a56a21abc21d950d04c83
apnan <aishajahan2015@gmail.com>
2022-10-06 01:22:22 -1200

    added margin in post

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -6,7 +6,7 @@ export default function Posts() {
     <div className="posts">
       <Post img="https://images.pexels.com/photos/228963/pexels-photo-228963.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
       <Post img="https://images.pexels.com/photos/9648783/pexels-photo-9648783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-      <Post img="https://images.pexels.com/photos/2884572/pexels-photo-2884572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+      <Post img="https://images.pexels.com/photos/4016509/pexels-photo-4016509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
       <Post img="https://images.pexels.com/photos/8734282/pexels-photo-8734282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
     </div>
   );
