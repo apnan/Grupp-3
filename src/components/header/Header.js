971481b342556ad65f464d9d94f3e1bf40d77ee4
apnan <aishajahan2015@gmail.com>
2022-10-05 12:43:40 -1200

    update header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,8 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">CANDY-CRUSH</span>
-        <span className="headerTitleLg">Find your perfect match</span>
+        <span className="headerTitleSm">LOREM-CRUSH</span>
       </div>
     </div>
   );
