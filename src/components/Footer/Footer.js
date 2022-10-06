0937a4226a6b297e05149d4ae44bff84a5729063
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-10-06 10:58:43 +0200

    testing deployment

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,21 @@
-import "./footer.css";
+import './footer.css';
 
 export default function Footer() {
   return (
     <div className="footer">
-    <span >FOLLOW US</span>
-    <div >
-      <i className="footerIcon fab fa-facebook-square"></i>
-      <i className="footerIcon fab fa-instagram-square"></i>
-      <i className="footerIcon fab fa-pinterest-square"></i>
-      <i className="footerIcon fab fa-twitter-square"></i>
-    </div> 
-     <hr />
-    <p className="footerItem">
-            &copy;{new Date().getFullYear()} All rights reserved |
-            Terms Of Service | Privacy
-          </p>
+      <span>FOLLOW US</span>
+      <div>
+        <i className="footerIcon fab fa-facebook-square"></i>
+        <i className="footerIcon fab fa-instagram-square"></i>
+        <i className="footerIcon fab fa-pinterest-square"></i>
+        <i className="footerIcon fab fa-twitter-square"></i>
+      </div>
+      <hr />
+      <p className="footerItem">
+        &copy;{new Date().getFullYear()} All rights reserved | Terms Of Service
+        | Privacy
+      </p>
     </div>
-  )
+  );
 }
+/* test */
